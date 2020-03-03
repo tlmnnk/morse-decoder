3278a2d6ef91529f4c7cc15dde1bf86a8f580f82
vladislav t <vt118@mail.ru>
2020-03-04 00:22:52 +0300

    feat: implemet function that comply with easy tests with spaces

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    const binaryMorse = {
+        '10':'.',
+        '11': '-'
+    };
+
+    const morseKeys = Object.keys(MORSE_TABLE);
+    const morseValues = Object.values(MORSE_TABLE);
+
+    let resultDotsAndDashes = '';
+    let morseLetter;
+    let resultString = '';
+    let morseArr = [];
+    
+    const wordsArrInDigits = expr.split('**********');
+    
+    if (wordsArrInDigits.length > 1) {
+        wordsArrInDigits.forEach((wordInDigits, i) => {
+            wordInDigits.match(/.{1,10}/g).forEach(letterInDigits => {
+
+                morseLetter = '';
+
+                letterInDigits.match(/.{1,2}/g).forEach(characterInDigits => {    
+                    Object.keys(binaryMorse).forEach(key => {
+
+                        if(key === characterInDigits) {
+                            morseLetter += binaryMorse[key];
+                        }
+                    });
+                });
+
+                morseArr.push(morseLetter);
+                
+            });
+
+            if( i + 1 !== wordsArrInDigits.length) {
+                morseArr.push(' ');
+            }
+                
+        });
+    }
+
+    morseArr.forEach(item => {
+        if(item === ' ') {
+            resultString += ' ';
+        }
+        morseKeys.forEach(key => {
+            if(key === item) {
+                resultString += MORSE_TABLE[key];
+            }
+        });
+    });
+
+
+    return resultString;
 }
 
 module.exports = {
