26386b6452e09662a2efd7534c16d786b6cad120
vladislav t <vt118@mail.ru>
2020-03-04 20:35:01 +0300

    feat: implement decode realization

--- src/index.js
+++ src/index.js
@@ -45,53 +45,63 @@ function decode(expr) {
     };
 
     const morseKeys = Object.keys(MORSE_TABLE);
-    const morseValues = Object.values(MORSE_TABLE);
 
-    let resultDotsAndDashes = '';
     let morseLetter;
     let resultString = '';
     let morseArr = [];
     
     const wordsArrInDigits = expr.split('**********');
+
+    function zerosToMorseWord(wordInDigits) {
+
+        wordInDigits.match(/.{1,10}/g).forEach(letterInDigits => {
+    
+            morseLetter = '';
+    
+            letterInDigits.match(/.{1,2}/g).forEach(characterInDigits => {    
+                Object.keys(binaryMorse).forEach(key => {
+    
+                    if(key === characterInDigits) {
+                        morseLetter += binaryMorse[key];
+                    }
+                });
+            });
+    
+            morseArr.push(morseLetter);      
+        });
+    }
+    
+    function checkForSpaces() {
+        morseArr.forEach(item => {
+            if(item === ' ') {
+                resultString += ' ';
+            }
+            morseKeys.forEach(key => {
+                if(key === item) {
+                    resultString += MORSE_TABLE[key];
+                }
+            });
+        });
+    }
     
     if (wordsArrInDigits.length > 1) {
-        wordsArrInDigits.forEach((wordInDigits, i) => {
-            wordInDigits.match(/.{1,10}/g).forEach(letterInDigits => {
+        wordsArrInDigits.forEach((wordInDigits, i) => {  
 
-                morseLetter = '';
-
-                letterInDigits.match(/.{1,2}/g).forEach(characterInDigits => {    
-                    Object.keys(binaryMorse).forEach(key => {
-
-                        if(key === characterInDigits) {
-                            morseLetter += binaryMorse[key];
-                        }
-                    });
-                });
-
-                morseArr.push(morseLetter);
-                
-            });
-
+            zerosToMorseWord(wordInDigits);
+            
             if( i + 1 !== wordsArrInDigits.length) {
                 morseArr.push(' ');
             }
                 
         });
-    }
 
-    morseArr.forEach(item => {
-        if(item === ' ') {
-            resultString += ' ';
-        }
-        morseKeys.forEach(key => {
-            if(key === item) {
-                resultString += MORSE_TABLE[key];
-            }
-        });
-    });
+        checkForSpaces();
 
-
+    } else {
+        zerosToMorseWord(expr);
+        checkForSpaces();
+  }
+        
     return resultString;
 }
 
